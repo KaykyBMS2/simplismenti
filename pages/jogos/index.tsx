ada6beca1d15d66a90a4b7e6f2a5cef6a9ddfbe4
KaykyBMS2 <enteredcssuprtbr@gmail.com>
2024-12-08 14:56:21 +0000

    cuida

--- pages/jogos/index.tsx
+++ pages/jogos/index.tsx
@@ -15,12 +15,7 @@ const Jogos: NextPage = () => {
         <div className="flex flex-col items-center justify-center ">
           <h1 className="font-bold text-xl mt-4">Jogos</h1>
           <ComingSoon />
-          <div className="mt-[30px] flex justify-center items-center flex-col">
-            <h1 className=" font-bold">Test Games</h1>
-              <div className="flex flex-col justify-center border-[2px] border-black items-center dark:bg-[#f8f8f8] bg-gray-300 rounded-2xl shadow-lg p-3 h-[229px] w-[159px]">
-                <Button className="mt-[70px]">Jogar</Button>
-              </div>
-          </div>
+          
           
         </div>
       </SignedIn>
