18f0e3fd83f12b3523f3325f9632c3ced376360e
KaykyBMS2 <enteredcssuprtbr@gmail.com>
2024-12-08 05:12:32 +0000

    Correcao de bugs _apps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
+import type { AppProps } from 'next/app'; // Importa o tipo correto do Next.js
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider appearance={{ baseTheme: dark }}>
       <Component {...pageProps} />
